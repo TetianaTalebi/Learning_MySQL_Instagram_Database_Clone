080c94adf98bb09249aaace5e0cff8ec0524f81e
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2025-06-10 13:06:18 -0400

    Create a table users in ig_clone.sql

--- ig_clone.sql
+++ ig_clone.sql
@@ -1,2 +1,11 @@
+
 CREATE DATABASE ig_clone;
 USE DATABASE ig_clone;
+
+CREATE TABLE users(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+
