0a6c0441e1472afae1349cc56cfdcc477a134f0a
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2025-06-10 15:06:16 -0400

    Create comments table in ig_clone.sql

--- ig_clone.sql
+++ ig_clone.sql
@@ -16,3 +16,13 @@ CREATE TABLE photos(
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
+
+CREATE TABLE comments(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
