bde32d312c172a5e25d7bb6217109eccea16c1a6
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2025-08-09 15:16:44 -0400

    Create table photo_tags

--- ig_clone.sql
+++ ig_clone.sql
@@ -53,3 +53,11 @@ CREATE TABLE tags(
     tag_name VARCHAR(255) UNIQUE,
     created_at TIMESTAMP DEFAULT NOW()
 );
+
+CREATE TABLE photo_tags(
+    photo_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id, tag_id)
+);
