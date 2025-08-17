4f6b6d13ad0de5c4a20d1aa49be151f24f1bad1d
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2025-08-17 19:15:44 -0400

    Add a solution to a problem no.2 Finding the most popular registration date in users table

--- solutions.sql
+++ solutions.sql
@@ -2,3 +2,14 @@
 
 SELECT * FROM users ORDER BY created_at ASC LIMIT 0,5;
 
+-- 2. Most popular registration date
+
+SELECT DAYNAME(created_at) AS day,
+       COUNT(*) AS total
+FROM users
+GROUP BY day
+ORDER BY total DESC LIMIT 0,1;
+
+
+
+
